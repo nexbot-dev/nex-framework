bb6abb4e8f020f2a7e40e1bdfb7331eb89b402ef
aenjojo <josuafernando999@gmail.com>
2023-07-11 21:26:22 +0700

    chore: add index export for registry

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './NexClient';
 export * from './core/Command';
 export * from './core/Event';
+export * from './core/Registry';
 export * from './libs/getRootPath';
 export * from './libs/readDirectory';
